87da62dab5519fc395832b2576db75dca6439f81
欧阳锋 <ouyangfeng2016@gmail.com>
2018-03-29 17:13:26 +0800

    Add library dependency and the logic of test library api.

--- app/src/main/java/com/youngfeng/jitpackdemo/MainActivity.java
+++ app/src/main/java/com/youngfeng/jitpackdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.youngfeng.jitpackdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.youngfeng.library.JitPack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JitPack.get().pack();
     }
 }
